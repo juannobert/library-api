f66f1cec20b1aec301057f39f29707e65e6ca86f
Juan Nobert <nobert407@gmail.com>
2022-08-10 15:42:12 -0300

    Entity Author created

---
+++ src/main/java/com/juannobert/library/api/entities/Author.java
@@ -0,0 +1,56 @@
+package com.juannobert.library.api.entities;
+
+import java.util.Objects;
+
+public class Author {
+
+	private Long id;
+	
+	private String name;
+	
+	public Author() {
+	}
+
+	public Author(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+}
