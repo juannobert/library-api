675d69005f6b3adfcf5670cda2a86750116bbcda
Juan Nobert <nobert407@gmail.com>
2022-08-10 19:09:19 -0300

    BCryptPasswordEncoder and web security config implemented

---
+++ src/main/java/com/juannobert/library/api/config/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.juannobert.library.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+public class WebSecurityConfig {
+
+	@Bean
+	WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring().antMatchers("/**");
+	}
+}
