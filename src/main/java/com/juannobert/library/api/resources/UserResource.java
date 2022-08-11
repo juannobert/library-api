eac2034648f64909859f92cd1b7ae7f07fde835d
Juan Nobert <nobert407@gmail.com>
2022-08-11 10:05:05 -0300

    User insert with password encoder implemented

--- src/main/java/com/juannobert/library/api/resources/UserResource.java
+++ src/main/java/com/juannobert/library/api/resources/UserResource.java
@@ -1,14 +1,20 @@
 package com.juannobert.library.api.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.juannobert.library.api.dto.UserDTO;
+import com.juannobert.library.api.dto.UserInsertDTO;
 import com.juannobert.library.api.services.UserService;
 
 @RestController
@@ -22,6 +28,16 @@ public class UserResource {
 	public ResponseEntity<Page<UserDTO>> findAllPaged(Pageable pageable){
 		Page<UserDTO> page = service.findAllPaged(pageable);
 		return ResponseEntity.ok(page);
+	}
+	
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto){
+		UserDTO userDTO = service.save(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(userDTO.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(userDTO);
+		
 	}
 
 }
--- src/main/java/com/juannobert/library/api/services/UserService.java
+++ src/main/java/com/juannobert/library/api/services/UserService.java
@@ -1,12 +1,15 @@
 package com.juannobert.library.api.services;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.juannobert.library.api.dto.UserDTO;
+import com.juannobert.library.api.dto.UserInsertDTO;
 import com.juannobert.library.api.entities.User;
 import com.juannobert.library.api.repositories.UserRepository;
 
@@ -16,10 +19,24 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	
 	@Transactional(readOnly = true)
 	public Page<UserDTO> findAllPaged(Pageable pageable) {
 		Page<User> page = repository.findAll(pageable);
 		return page.map(x -> new UserDTO(x));
+	}
+	
+	@Transactional
+	public UserDTO save(UserInsertDTO dto) {
+		User entity = new User();
+		BeanUtils.copyProperties(dto, entity,"id","password");
+		entity.setPassword(passwordEncoder.encode(dto.getPassword()));
+		entity = repository.save(entity);
+		return new UserDTO(entity);
+		
 	}
 	
 	
