3c6aa54cab2905f486e3930f5951569b002757ea
Juan Nobert <nobert407@gmail.com>
2022-08-11 15:10:39 -0300

    Categories find all paged implemented

---
+++ src/main/java/com/juannobert/library/api/resources/CategoryResource.java
@@ -0,0 +1,29 @@
+package com.juannobert.library.api.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.juannobert.library.api.dto.CategoryDTO;
+import com.juannobert.library.api.services.CategoryService;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryResource {
+	
+	@Autowired
+	private CategoryService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<CategoryDTO>> findAllPaged(Pageable pageable){
+		Page<CategoryDTO> page = service.findAllPaged(pageable);
+		return ResponseEntity.ok(page);
+	}
+	
+	
+
+}
