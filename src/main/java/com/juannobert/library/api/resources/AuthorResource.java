56e3dedb9515a88b7d2c0a5cc546317b52de2f60
Juan Nobert <nobert407@gmail.com>
2022-08-12 10:00:24 -0300

    Insert Auhtor implemented

--- src/main/java/com/juannobert/library/api/resources/AuthorResource.java
+++ src/main/java/com/juannobert/library/api/resources/AuthorResource.java
@@ -1,12 +1,17 @@
 package com.juannobert.library.api.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.juannobert.library.api.dto.AuthorDTO;
 import com.juannobert.library.api.services.AuthorService;
@@ -24,6 +29,13 @@ public class AuthorResource {
 		return ResponseEntity.ok(page);
 	}
 	
-	
+	@PostMapping
+	public ResponseEntity<AuthorDTO> insert(@RequestBody AuthorDTO dto){
+		AuthorDTO userDTO = service.save(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(userDTO.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(userDTO);	
+	}
 
 }
--- src/main/java/com/juannobert/library/api/services/AuthorService.java
+++ src/main/java/com/juannobert/library/api/services/AuthorService.java
@@ -1,5 +1,6 @@
 package com.juannobert.library.api.services;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,6 +22,14 @@ public class AuthorService {
 	public Page<AuthorDTO> findAllPaged(Pageable pageable) {
 		Page<Author> page = repository.findAll(pageable);
 		return page.map(x -> new AuthorDTO(x));
+	}
+	
+	
+	public AuthorDTO save(AuthorDTO dto) {
+		Author entity = new Author();
+		BeanUtils.copyProperties(dto, entity,"id");
+		entity = repository.save(entity);
+		return new AuthorDTO(entity);
 	}
 	
 	
