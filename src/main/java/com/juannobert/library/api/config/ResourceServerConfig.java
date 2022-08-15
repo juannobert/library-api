024f7f993a7d020da28455bb567fc6912ad736b6
Juan Nobert <nobert407@gmail.com>
2022-08-15 19:00:03 -0300

    Resource server config implemented

---
+++ src/main/java/com/juannobert/library/api/config/ResourceServerConfig.java
@@ -0,0 +1,56 @@
+package com.juannobert.library.api.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private JwtTokenStore tokenStore;
+	public static final String[] PUBLIC = {"/oauth/token","/h2-console/**","/books/**"};
+	
+	public static final String[] OPERATOR_OR_ADMIN = {"/loans/**","/books/**","/authors/**"};
+	
+	public static final String[] OPERATOR_GET = {"/users/**"};
+	
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(tokenStore);
+	}
+	
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) { 
+			http.headers().frameOptions().disable(); 
+		}
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET,PUBLIC).permitAll()
+		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).authenticated()
+		.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN") 
+		.antMatchers(HttpMethod.GET,OPERATOR_GET).hasAnyRole("OPERATOR")
+		.anyRequest().hasAnyRole("ADMIN");
+
+	}
+	
+	
+	
+	
+	
+	
+}
